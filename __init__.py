0405b6db2d58e5cacc749852d1cab665b7ea35ce
tin2tin <tintwotin@gmail.com>
2023-03-27 10:56:03 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@ bl_info = {
 
 import bpy, os
 from bpy_extras.io_utils import ExportHelper
+from bpy_extras.io_utils import ImportHelper
 from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
 
@@ -237,6 +238,139 @@ def menu_func_export(self, context):
     )
 
 
+# Class by Meta Person https://github.com/metaperson/blender_vse/blob/main/tube/20210924%231/addon_import_strips.py
+class SEQUENCER_OT_import_strips(bpy.types.Operator, ImportHelper):
+    bl_description = 'import strips'
+    bl_idname = 'sequencer.import_strips'
+    bl_label = 'Import'
+
+    files: bpy.props.CollectionProperty(name='Import Strips', type=bpy.types.OperatorFileListElement)
+
+    order_by: bpy.props.EnumProperty(name='Import Order',
+                description='Strips are sorted by this order',
+                items=[('PICK',         'Pick',                 'No Sort, as selected order'),
+                       ('CREATE_TIME',  'File Create Time',     'Sort order by the file created time'),
+                       ('FILE_NAME',    'File Name',            'Sort order by the file name'),
+                       ('FILE_SIZE',    'File Size',            'Sort order by the file size')],
+                default='FILE_NAME')
+
+    reversed_order: bpy.props.BoolProperty(name='Reversed order', description='Reversed order for sorting', default=False)
+    
+    channel: bpy.props.IntProperty(name='Channel', description='Assign channel to put strips', default=1, min=1)
+
+    image_strip_length: bpy.props.IntProperty(name='Image Duration', description='Image strip length', default=25, min=1)
+    
+    fit_method: bpy.props.EnumProperty(name='Scale',
+                description='Scale fit method',
+                items=[('FIT',          'Scale to Fit',         'Scale image to fit within the canvas'),
+                       ('FILL',         'Scale to Fill',        'Scale image to completely fill the canvas'),
+                       ('STRETCH',      'Stretch to Fill',      'Stretch image to fill the canvas'),
+                       ('ORIGINAL',     'Use Original Size',    'Keep image at its original size')],
+                default='FIT')
+
+
+    @classmethod
+    def poll(cls, context):
+        if not bpy.ops.sequencer.movie_strip_add.poll():
+            return False
+        if not bpy.ops.sequencer.sound_strip_add.poll():
+            return False
+        if not bpy.ops.sequencer.image_strip_add.poll():
+            return False
+        return True
+
+    def execute(self, context):
+        #print('ImportHelper.files : {}'.format(self.files))
+        #print('file path : {}'.format(self.filepath))
+        #print('Sort by order : {}'.format(self.order_by))
+        
+        strip_dirname = os.path.dirname(self.filepath)
+        strip_files = self.files
+
+        #for strip_file in strip_files:
+        #    print(strip_file)
+
+        if self.order_by == 'PICK':
+            if self.reversed_order:
+                strip_files = reversed(strip_files)
+            else:
+                strip_files = list(strip_files)
+        elif self.order_by == 'CREATE_TIME':
+            strip_files = sorted(strip_files, key=lambda x: os.path.getctime(os.path.join(strip_dirname, x.name)), reverse=self.reversed_order)
+        elif self.order_by == 'FILE_NAME':
+            strip_files = sorted(strip_files, key=lambda x: x.name, reverse=self.reversed_order)
+        elif self.order_by == 'FILE_SIZE':
+            strip_files = sorted(strip_files, key=lambda x: os.path.getsize(os.path.join(strip_dirname, x.name)), reverse=self.reversed_order)
+        else:
+            return {'CANCELLED'}
+
+        # for strip_file in strip_files:
+        #     print(strip_file)
+
+        count_movie = 0
+        count_sound = 0
+        count_image = 0
+        
+        for strip_file in strip_files:
+            strip_ext = os.path.splitext(strip_file.name)[1].lower()
+            # print(strip_dirname, strip_file.name, strip_ext)
+            frame_start = max([seq.frame_final_end for seq in context.sequences] or [0])
+            if strip_ext in ('.avi', '.mp4', '.mpg', '.mpeg', '.mov', '.mkv', '.dv', '.flv'):
+                strip_path = os.path.join(strip_dirname, strip_file.name)
+                bpy.ops.sequencer.movie_strip_add(filepath=strip_path,
+                                                  frame_start=frame_start,
+                                                  channel=self.channel,
+                                                  fit_method=self.fit_method)
+                count_movie += 1
+            elif strip_ext in ('.acc', '.ac3', '.flac', '.mp2', '.mp3', '.m4a','.pcm', '.ogg'):
+                strip_path = os.path.join(strip_dirname, strip_file.name)
+                bpy.ops.sequencer.sound_strip_add(filepath=strip_path,
+                                                  frame_start=frame_start,
+                                                  channel=self.channel)
+                count_sound += 1
+            elif strip_ext in ('.jpg', '.jpeg', '.bmp', '.png', '.gif', '.tga', '.tiff'):
+                bpy.ops.sequencer.image_strip_add(directory=strip_dirname + '\\', files=[{"name":strip_file.name, "name":strip_file.name}],
+                                                  show_multiview=False,
+                                                  frame_start=frame_start, frame_end=frame_start+self.image_strip_length,
+                                                  channel=self.channel,
+                                                  fit_method=self.fit_method)
+                count_image += 1
+                                                  
+        self.report({'INFO'}, 'Imported Movie[{}], Sound[{}], Image[{}], Total[{}]'.format(count_movie,
+                                                                                           count_sound,
+                                                                                           count_image,
+                                                                                           count_movie + count_sound + count_image))
+
+
+        return {'FINISHED'}
+    
+#    def draw(self, context):
+#        layout = self.layout()
+#        layout.label("hej")
+#        #layout.operator("SEQUENCER_OT_sound_strip_add", text="Cache")
+        
+
+#class SEQUENCER_PT_import(bpy.types.Panel):
+#    bl_space_type = "FILE_BROWSER"
+#    bl_region_type = "TOOL_PROPS"
+#    bl_label = "Import Options"
+#    bl_parent_id = "FILE_PT_operator"
+
+#    @classmethod
+#    def poll(cls, context):
+#        sfile = context.space_data
+#        operator = sfile.active_operator
+#        return operator.bl_idname == "SEQUENCER_OT_import_strips"
+
+#    def invoke(self, context, event):
+#        context.scene.render.use_sequencer = True
+
+#    def draw(self, context):
+#        layout = self.layout()
+#        layout.operator("SEQUENCER_OT_sound_strip_add.cache", text="Cache")
+#        pass
+
+
 class SEQUENCER_MT_sequence(bpy.types.Menu):
     bl_idname = "SEQUENCER_MT_sequence"
     bl_label = "Sequence"
@@ -249,14 +383,11 @@ class SEQUENCER_MT_sequence(bpy.types.Menu):
         layout.operator_context = "INVOKE_DEFAULT"
 
         layout.separator()
-        layout.operator("sequencer.movie_strip_add", text="Import Movie")
-        layout.operator("sequencer.sound_strip_add", text="Import Sound")
-        layout.operator("sequencer.image_strip_add", text="Import Image/Sequence")
+        layout.operator("sequencer.import_strips", text="Import")
+
+        props = layout.operator("sequencer.export", text="Export")
 
         layout.separator()
-        props = layout.operator("sequencer.export", text="Export Sequence")
-        # props.animation = True
-        # props.sequencer = True
         layout.operator("render.opengl", text="Render Image").sequencer = True
 
         layout.separator()
@@ -268,7 +399,6 @@ class SEQUENCER_MT_sequence(bpy.types.Menu):
 
 def prepend_sequence_menu(self, context):
     self.layout.menu("SEQUENCER_MT_sequence")
-    # self.layout.separator()
 
 
 classes = (
@@ -282,6 +412,8 @@ classes = (
     SEQUENCER_PT_export_time_stretching,
     SEQUENCER_PT_export_post_processing,
     SEQUENCER_PT_export_folder_browser,
+    SEQUENCER_OT_import_strips,
+    #SEQUENCER_PT_import,
     SEQUENCER_MT_sequence,
 )
 
